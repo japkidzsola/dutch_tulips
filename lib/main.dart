af7c9af449695dc12af34acc95965da54a2d4f4e
Japkidzsola <suzukipontzsolt@gmail.com>
2025-02-10 21:33:34 +0100

    created tulipProduct class

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,20 @@ void main() {
   runApp(const MyApp());
 }
 
+class TulipProduct {
+  final String name;
+  final String description;
+  final double price;
+  final String imageUrl;
+
+  const TulipProduct({
+    required this.name,
+    required this.description,
+    required this.price,
+    required this.imageUrl,
+  });
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
